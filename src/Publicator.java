505f95bdd4629e02c128fb4df830b6fe860a0445
Evgeny-Bukovski <eugene@codrob.ru>
2017-09-04 09:34:52 +0300

    Publication

--- src/Publicator.java
+++ src/Publicator.java
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Created by Eugene on 02.09.2017.
  */
 public class Publicator extends Thread {
@@ -11,20 +11,11 @@ public class Publicator extends Thread {
         while (Console.getStatusProgram()) {
             News tmpNews =  DataBase.getWaitPublication();
             if (tmpNews != null) {
-                //Написать реальную публикацию
-                //System.out.println(tmpNews.subId + " | " + tmpNews.getTitle());
+                /*
+		Secret Section
+		*/
                 DataBase.putLastNews(tmpNews);
             }
        }
-
-        //FOR DEBUG
-        /*HashMap<Integer, List<News>> tmp = new HashMap<>();
-        for (News tmpNews : dequeWaitPublication) {
-            List<News> tmpList = new ArrayList<>();
-            tmpList.add(tmpNews);
-            if (tmp.containsKey(tmpNews.subId))
-                tmpList.addAll(tmp.get(tmpNews.subId));
-            tmp.put(tmpNews.subId, tmpList);
-        }*/
     }
 }
